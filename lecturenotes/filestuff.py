3400287dee507c4d63d5313fca02415de7b11e5a
tehepix2 <daniel12landa@gmail.com>
2024-11-14 13:24:07 -0500

    file stuff

---
+++ lecturenotes/filestuff.py
@@ -0,0 +1,5 @@
+# initialize a file object
+my_file = open("myfile.txt", 'r') # opens a file in the current directory if the absolute path is not specified
+                                  # mode 'r' means it is in read mode, it can only be read
+
+# interact with the file
